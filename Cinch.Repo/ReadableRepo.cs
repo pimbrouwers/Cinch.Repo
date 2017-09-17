bdd536e30882ade3c7157a619789333a4077f5c5
Pim Brouwers <pbrouwers@nhlpa.com>
2017-09-17 14:35:44 -0400

    Query() / First() moved into ReadableRepo

--- Cinch.Repo/ReadableRepo.cs
+++ Cinch.Repo/ReadableRepo.cs
@@ -1,5 +1,8 @@
-﻿using Dapper.Contrib.Extensions;
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cinch.Repo
@@ -15,6 +18,32 @@ namespace Cinch.Repo
                 return await conn.GetAsync<TEntity>(id);
             }
         }
-        
+
+        /// <summary>
+        /// Get Many
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="commandType"></param>
+        /// <returns>Enumerable of TEntity</returns>
+        public async Task<IEnumerable<TEntity>> Query(string sql, object param = null, CommandType commandType = CommandType.Text)
+        {
+            using (var conn = await connectionFactory.CreateConnection())
+            {
+                return await conn.QueryAsync<TEntity>(sql, param, commandType: commandType);
+            }
+        }
+
+        /// <summary>
+        /// Get First
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <param name="commandType"></param>
+        /// <returns>TEntity or default(TEntity)</returns>
+        public async Task<TEntity> First(string sql, object param = null, CommandType commandType = CommandType.Text)
+        {
+            return (await Query(sql, param, commandType)).FirstOrDefault();
+        }
     }
 }
--- Cinch.Repo/Repo.cs
+++ Cinch.Repo/Repo.cs
@@ -2,7 +2,6 @@
 using Dapper.Contrib.Extensions;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Cinch.Repo
@@ -102,33 +101,6 @@ namespace Cinch.Repo
             {
                 return await conn.ExecuteAsync(sql, param, commandType: commandType);
             }
-        }
-
-        /// <summary>
-        /// Get Many
-        /// </summary>
-        /// <param name="sql"></param>
-        /// <param name="param"></param>
-        /// <param name="commandType"></param>
-        /// <returns>Enumerable of TEntity</returns>
-        public async Task<IEnumerable<TEntity>> Query(string sql, object param = null, CommandType commandType = CommandType.Text)
-        {
-            using (var conn = await connectionFactory.CreateConnection())
-            {
-                return await conn.QueryAsync<TEntity>(sql, param, commandType: commandType);
-            }
-        }
-
-        /// <summary>
-        /// Get First
-        /// </summary>
-        /// <param name="sql"></param>
-        /// <param name="param"></param>
-        /// <param name="commandType"></param>
-        /// <returns>TEntity or default(TEntity)</returns>
-        public async Task<TEntity> First(string sql, object param = null, CommandType commandType = CommandType.Text)
-        {
-            return (await Query(sql, param, commandType)).FirstOrDefault();
-        }
+        }       
     }
 }
